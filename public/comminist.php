0d582254da78da4f43811ecd294763e3cf41434d
devvoh <hello@devvoh.com>
2014-11-25 23:58:09 +0100

    FIX: finally got around to fixing the messy config init FIX: added some file_exist checks on src files and relevant error dies

--- public/comminist.php
+++ public/comminist.php
@@ -2,32 +2,36 @@
 <?php
 /**
  * Communist - php css combiner/minifier
+ * 
+ * devvoh, 2014
  */
 
 // Check for parameters
-if(isset($argv[1]) && $argv[1] === 'init') {
-    $config  = '{' . PHP_EOL;
-    $config .= '    "1": {' . PHP_EOL;
-    $config .= '        "task": "combine",' . PHP_EOL;
-    $config .= '        "src": [' . PHP_EOL;
-    $config .= '            "../file1.ext",' . PHP_EOL;
-    $config .= '            "../file2.ext"' . PHP_EOL;
-    $config .= '        ],' . PHP_EOL;
-    $config .= '        "dest": "./combined.css"' . PHP_EOL;
-    $config .= '    },' . PHP_EOL;
-    $config .= '    "2": {' . PHP_EOL;
-    $config .= '        "task": "minify",' . PHP_EOL;
-    $config .= '        "src": "./combined.css",' . PHP_EOL;
-    $config .= '        "dest": "./combined.min.css"' . PHP_EOL;
-    $config .= '    },' . PHP_EOL;
-    $config .= '    "3": {' . PHP_EOL;
-    $config .= '        "task": "delete",' . PHP_EOL;
-    $config .= '        "src": "./combined.css"' . PHP_EOL;
-    $config .= '    }' . PHP_EOL;
-    $config .= '}';
+if (isset($argv[1]) && $argv[1] === 'init') {
+    $config = array(
+        '1' => array(
+            'task' => 'combine',
+            'src' => array(
+                './file1.css',
+                './file2.css'
+            ),
+            'dest' => './combined.css'
+        ),
+        '2' => array(
+            'task' => 'minify',
+            'src' => './combined.css',
+            'dest' => './combined.min.css'
+        ),
+        '3' => array(
+            'task' => 'delete',
+            'src' => './combined.css'
+        )
+    );
     
+    $config = json_encode($config, JSON_PRETTY_PRINT);
     file_put_contents('./comminist.config.json', $config);
-    die('blank comminist.config.json generated.' . PHP_EOL );
+    // Die before we run this broken example config
+    die('blank comminist.config.json generated.' . PHP_EOL);
 }
 
 // Actually instantiate & run class
@@ -35,47 +39,62 @@ $cm = new Communist();
 
 class Communist
 {
+
     protected $version = '0.1.0';
+
     protected $configFilename = 'comminist.config.json';
+
     protected $config = array();
-    
-    public function __construct() {
+
+    public function __construct()
+    {
         $this->loadConfig();
     }
-    
-    protected function loadConfig() {
+
+    protected function loadConfig()
+    {
         // Get & json_decode config
         if (is_file($this->configFilename)) {
             echo 'comminist.config.json found, executing...' . PHP_EOL . PHP_EOL;
             $this->config = json_decode(file_get_contents('./' . $this->configFilename));
             $this->execute();
         } else {
-            die('ERROR: no comminist.config.json file found, run \'comminist init\' to generate a blank one.' . PHP_EOL );
+            die('ERROR: no comminist.config.json file found, run \'comminist init\' to generate a blank one.' . PHP_EOL);
         }
     }
-    
-    protected function execute() {
-    
+
+    protected function execute()
+    {
         foreach ($this->config as $task => $params) {
             $temp = '';
             
             switch ($params->task) {
-            
+                
                 case 'combine':
+                    
                     // Loop and store in memory
-                    foreach($params->src as $filename) {
-                        $temp .= file_get_contents($filename);
+                    foreach ($params->src as $filename) {
+                        if (is_file($filename)) {
+                            $temp .= file_get_contents($filename);
+                        } else {
+                            die('ERROR: to be combined file ' . $filename . ' does not exist.' . PHP_EOL);
+                        }
                     }
                     
                     // And save
                     file_put_contents($params->dest, $temp);
                     
-                    $this->debug('combined... ' . $params->dest);
-                break;
+                    $this->debug('  combined... ' . $params->dest);
+                    break;
                 
                 case 'minify':
+                    
                     // Get file content
-                    $temp = file_get_contents($params->src);
+                    if (is_file($params->src)) {
+                        $temp = file_get_contents($params->src);
+                    } else {
+                        die('ERROR: to be minified file ' . $filename . ' does not exist.' . PHP_EOL);
+                    }
                     
                     $lengthBefore = strlen($temp);
                     
@@ -84,40 +103,48 @@ class Communist
                     // Remove space after colons
                     $temp = str_replace(': ', ':', $temp);
                     // Remove whitespace
-                    $temp = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', $temp);
-
+                    $temp = str_replace(array(
+                        "\r\n",
+                        "\r",
+                        "\n",
+                        "\t",
+                        '  ',
+                        '    ',
+                        '    '
+                    ), '', $temp);
+                    
                     $lengthAfter = strlen($temp);
                     
-                    if($lengthBefore > 0) {
-                        $percentage = number_format(
-                            100 - (100 / $lengthBefore) * $lengthAfter,
-                            2
-                        );
+                    $percentage = 0;
+                    if ($lengthBefore > 0) {
+                        $percentage = number_format(100 - (100 / $lengthBefore) * $lengthAfter, 2);
                     }
                     
                     // And save
                     file_put_contents($params->dest, $temp);
                     
-                    $this->debug('minified... ' . $params->dest . ' saved ' . $percentage . '%...');
-                break;
+                    $this->debug('  minified... ' . $params->dest . ' saved ' . $percentage . '%...');
+                    break;
                 
                 case 'delete':
                     if (is_file($params->src)) {
-                        unlink($params->src);
-                        $this->debug('deleted ' . $params->src . '...');
+                        if (unlink($params->src)) {
+                            $this->debug('   deleted... ' . $params->src . '...');
+                        } else {
+                            $this->debug('couldn\'t delete ' . $params->src . ' for unknown reason, check permissions...');
+                        }
                     } else {
-                        $this->debug('couldn\'t delete ' . $params->src . '...');
+                        $this->debug('couldn\'t delete ' . $params->src . ' because it didn\'t exist...');
                     }
-                break;
-                
+                    break;
             }
         }
         
         echo PHP_EOL . 'Done!' . PHP_EOL;
-        
     }
-    
-    public function debug($message) {
+
+    public function debug($message)
+    {
         echo $message;
         echo PHP_EOL;
     }
